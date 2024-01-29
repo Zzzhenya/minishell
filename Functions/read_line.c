5e42b39eab05442ba249c37a9cdc1bd8fbea6ebf
Shenya De Silva <shenya44@outlook.com>
2024-01-29 18:48:45 +0100

    Initial commit readline functions

---
+++ Functions/read_line.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+//#include <readline/history.h>
+
+
+//char *readline (char *prompt);
+
+
+int main (void)
+{
+	char *line = readline ("Enter a line: ");
+
+	printf("User > %s\n", line);
+	free (line);
+	return (0);
+}
