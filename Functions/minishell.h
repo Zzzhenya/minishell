44c1323b895ff289a46a3219c7acf8787cc503ce
Taekeundo <pleasepose@gmail.com>
2024-02-13 21:24:05 +0100

    update headfile

--- Functions/minishell.h
+++ Functions/minishell.h
@@ -1,16 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkwak <tkwak@student.42berlin.de>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/13 21:10:59 by tkwak             #+#    #+#             */
+/*   Updated: 2024/02/13 21:19:32 by tkwak            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <stdio.h>
-# include <stdlib.h>
-/* READLINE includes + flags in Makefile */
-# include <readline/readline.h>
-# include <readline/history.h>
-/* execve fork */
+// close, read, write, access, dup, dup2, execve, fork, pipe, unlink
+// STDIO_FILENO, STDOUT_FILENO, getcwd, chdir, isatty, ttyname, ttyslot
 # include <unistd.h>
-/* wait wait2 wait3*/
-#include <sys/wait.h>
 
-char	**ft_splitbyspace(char *str);
+// malloc, free, exit, getenv
+# include <stdlib.h>
+
+// printf, perror
+# include <stdio.h>
+
+// opendir, readdir, closedir
+# include <dirent.h>
+
+// open, O_RDONLY, O_WRONLY, O_RDWR, O_CREAT, O_TRUNC
+# include <fcntl.h>
+
+// errno, EACCES, ENOENT
+# include <errno.h>
+
+// strerror
+# include <string.h>
+
+// readline
+# include <readline/readline.h>
+
+// add_history, clear_history
+# include <readline/history.h>
+
+// signal, kill, SIGINT, SIGQUIT, sigaction, sigemptyset, sigaddset
+# include <signal.h>
+
+// wait, waitpid, wait3, wait4
+# include <sys/wait.h>
+
+// stat, lstat, fstat
+# include <sys/stat.h>
+
+// ioctl
+# include <sys/ioctl.h>
+
+// tcsetattr, tcgetattr
+# include <termios.h>
+
+// tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
+# include <curses.h>
+# include <term.h>
+
+// @note FLAGS
+// STDIN_FILENO == 0
+// STDOUT_FILENO == 1
+// STDERR_FILENO == 2
+# define PIPE_FLAG 3
+# define REDI_WR_APPEND_FLAG 4
+# define REDI_WR_TRUNC_FLAG 5
+# define REDI_RD_FLAG 6
+# define HDOC_FLAG 7
+
+# include "../libft/include/libft.h"
+# include "../libft/include/ft_printf.h"
+# include "pipex.h"
 
 #endif
---
+++ Functions/parsing/main.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkwak <tkwak@student.42berlin.de>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/12 15:25:32 by tkwak             #+#    #+#             */
+/*   Updated: 2024/02/12 15:25:38 by tkwak            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h> // printf
+# include <stdlib.h> // malloc, free, exit, getenv
+# include <unistd.h> //getcwd, chdir, fork, execve, wait, write, read, pipe, dup, dup2, close, isatty, ttyname, ttyslot, unlink
+# include <string.h> // strerror, perror
+# include <sys/wait.h> // waitpid, wait3, wait4
+# include <sys/stat.h> // stat, lstat, fstat, access, open, read, close
+# include <fcntl.h> // open, read, close, access
+# include <errno.h>
+# include <readline/readline.h> // readline
+# include <readline/history.h> // history
+# include <signal.h> // signal, sigaction, sigemptyset, sigaddset, kill
+# include <dirent.h> // opendir, closedir, readdir
+# include <sys/ioctl.h> // ioctl
+# include <termios.h> // tcgetattr, tcsetattr
+# include <ncurses.h> // tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
+
+//define el tipo de token
+# define T_NULL 0
+# define WORD 1
+# define PIPE 2
+# define REDIR 3
+# define DOUBLE_QUOTES 4
+# define SINGLE_QUOTES 5
+
+//estructura para un token
+typedef struct  s_token 
+{
+
+} t_token;
+
+int main();
+
+#endif
