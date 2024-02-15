72c1f3f076662a0c9fbc1cc55660abcf55921d70
Shenya De Silva <shenya44@outlook.com>
2024-02-15 14:13:21 +0100

    Initial commit built_in.c

---
+++ Shenya/Functions/built_in.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main (int ac, char **av, char **envp)
+{
+	int i = 0;
+	if (ac == 1)
+	{
+		printf("%s\n",av[0]);
+		while (envp[i])
+		{
+			printf("%s\n", envp[i]);
+			i ++;
+		}
+	}
+	return (0);
+}
