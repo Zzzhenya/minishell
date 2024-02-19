845b682a99e9c2daa37bd1a84b06459d4069c514
Shenya De Silva <shenya44@outlook.com>
2024-02-19 16:11:41 +0100

    Main function has three arguments

--- Shenya/Functions/main.c
+++ Shenya/Functions/main.c
@@ -2,14 +2,10 @@
 
 //char *readline (char *prompt);
 //char **envp
-int main (void)
-{
-	char 	*line; 
-	char 	**argv;
-	int 	argc;
 
-	line = NULL;
-	argv = NULL;
+void interactive_bash(char **argv, char *line, int argc, char **envp)
+{
+	(void)envp;
 	while (1)
 	{
 		line = readline ("Shell % ");
@@ -20,6 +16,25 @@ int main (void)
 		//get_env_var(argv[0]);
 		clean_argv(argv, argc);
 		free (line);
+	}
+}
+
+void	non_interactive_bash(char *arg, char **envp)
+{
+	(void)envp;
+	(void)arg;
+}
+
+
+int main (int argc, char **argv, char **envp)
+{
+	if (argc == 2)
+		non_interactive_bash(argv[1], envp);
+	else
+	{
+		(void)argc;
+		(void)argv;
+		interactive_bash(NULL, NULL, 0, envp);
 	}
 	//system("leaks read_line"); 
 	return (0);
