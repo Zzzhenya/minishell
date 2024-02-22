83fd678862596025f45582242535d196cf84253a
Shenya De Silva <sde-silv@c4b12c5.42berlin.de>
2024-02-22 16:35:32 +0100

    tty update for stdout and strderr

--- Shenya/Functions/Tests/tty.c
+++ Shenya/Functions/Tests/tty.c
@@ -2,25 +2,25 @@
 #include <unistd.h>
 #include <sys/errno.h>
 
+#define STDIN 0
+#define STDOUT 1
+#define STDERR 2
+
 //extern int errno;
 
 int main (void)
 {
 	//extern int errno;
-	printf("%d\n", errno);
-	if (isatty(0) == 1)
-	{
-		printf("%d\n", errno);
-		printf("ISATTY\n");
-	}
+	printf("initial errno: %d\n", errno);
+	if (isatty(STDIN) && isatty(STDOUT) && isatty(STDERR))
+		printf("ISATTY %d\n", errno);
+	else
+		printf("erno on failure fd==%d,%d,%d: %d\n",STDIN, STDOUT, STDERR, errno);
 	if (isatty(5) == 1)
-	{
-		printf("%d\n", errno);
-		printf("ISATTY\n");
-	}
+		printf("ISATTY %d\n", errno);
 	else
 	{
-		printf("%d\n", errno);
+		printf("erno on failure fd==5: %d\n", errno);
 		return (errno);
 	}
 	return (0);
