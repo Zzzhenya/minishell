763165f1d33500bdfa452abb9f8cefdb08153264
Shenya De Silva <sde-silv@c4b11c5.42berlin.de>
2024-02-27 19:29:22 +0100

    Removed extra buffer and added details from man bash

--- Shenya/Functions/exec_builtin.c
+++ Shenya/Functions/exec_builtin.c
@@ -1,26 +1,58 @@
 #include "minishell.h"
 
-void	exec_pwd(void)
-{
-	char *buf;
-	char *retptr;
+/*
+man bash
 
-	buf = malloc(sizeof(char) * 1024);
-	if(!buf)
-		printf("malloc failed\n");
-	buf[1024] = '\0';
-	retptr = getcwd(buf, 1024);
-	if (!retptr)
+ EXIT STATUS
+       The exit status of an executed command is the value returned by the  waitpid  system  call  or  equivalent
+       function.   Exit  statuses  fall  between  0 and 255, though, as explained below, the shell may use values
+       above 125 specially.  Exit statuses from shell builtins and compound commands are  also  limited  to  this
+       range.  Under certain circumstances, the shell will use special values to indicate specific failure modes.
+
+       For  the shell's purposes, a command which exits with a zero exit status has succeeded.  An exit status of
+       zero indicates success.  A non-zero exit status indicates failure.  When a command terminates on  a  fatal
+       signal N, bash uses the value of 128+N as the exit status.
+
+       If  a command is not found, the child process created to execute it returns a status of 127.  If a command
+       is found but is not executable, the return status is 126.
+
+       If a command fails because of an error during expansion or redirection, the exit status  is  greater  than
+       zero.
+
+       Shell  builtin commands return a status of 0 (true) if successful, and non-zero (false) if an error occurs
+       while they execute.  All builtins return an exit status of 2 to indicate incorrect  usage,  generally  in‐
+       valid options or missing arguments.
+
+       Bash  itself  returns the exit status of the last command executed, unless a syntax error occurs, in which
+       case it exits with a non-zero value.  See also the exit builtin command below.
+
+       pwd [-LP]
+              Print  the  absolute  pathname  of the current working directory.  The pathname printed contains no
+              symbolic links if the -P option is supplied or the -o physical option to the set builtin command is
+              enabled.   If  the  -L option is used, the pathname printed may contain symbolic links.  The return
+              status is 0 unless an error occurs while reading the name of the current directory  or  an  invalid
+              option is supplied.
+
+*/
+
+#define PATH_MAX 4
+//#include <limits.h>
+
+void	exec_pwd(void)
+{  
+	char *buf;
+
+    buf = NULL;
+	buf = getcwd(buf, PATH_MAX);
+	if (!buf)
+    {
+        exit(EXIT_FAILURE);
+        // set error no value
+    }
+    else
 	{
-		free(buf);
-		printf("getcwd crashed\n");
-	}
-	else
-	{
-        // Line break after printing to the terminal
-        // How to print to terminal?
-		printf("%s\n", retptr);
-		free (retptr);
+		printf("%s\n", buf);
+		free (buf);
 	}
 }
 
