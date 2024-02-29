28c0f5b2462bd929e37516f3171a20199be4072c
Shenya De Silva <shenya44@outlook.com>
2024-02-29 19:01:16 +0100

    Initial commit pwd

---
+++ Shenya/Functions/builtins/pwd.c
@@ -0,0 +1,47 @@
+#include "../minishell.h"
+
+char	*get_pwd(void)
+{
+	char *path;
+
+    path = NULL;
+	path = getcwd(path, PATH_MAX);
+	if (!path)
+	{
+		g_exit_status = EX_CMD_NOT_FOUND;
+        return (NULL);
+	}
+    else
+		return (path);
+}
+
+/* When executing pwd should I exit when an error occurs
+ and/or when successfully executed?
+
+ is 127 the correct exit code for when getcwd fails? 
+ */
+
+
+
+void	exec_pwd(void)
+{  
+	char *path;
+
+    path = NULL;
+	path = getcwd(path, PATH_MAX);
+	if (!path)
+	{
+		g_exit_status = EX_CMD_NOT_FOUND;
+		return;
+        //exit(g_exit_status);
+	}
+    else
+	{
+		ft_putstr_fd(path, 1);
+		ft_putchar_fd('\n', 1);
+		free (path);
+	}
+	g_exit_status = EXIT_SUCCESS;
+	return;
+	//exit(g_exit_status);
+}
