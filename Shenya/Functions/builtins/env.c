a1008eb783a55614752a878f25c10802a6644d48
Shenya De Silva <shenya44@outlook.com>
2024-03-10 20:22:39 +0100

    Adjusted the function to print content from envlist instead of envarr * ?

--- Shenya/Functions/builtins/env.c
+++ Shenya/Functions/builtins/env.c
@@ -2,6 +2,7 @@
 
 void	exec_env(char **argv, t_envp *my_data)
 {
+	/*
 	int i = 0;
 	(void)argv;
 	while (my_data->envp[i] != NULL)
@@ -9,5 +10,15 @@ void	exec_env(char **argv, t_envp *my_data)
 		ft_putstr_fd(my_data->envp[i], 1);
 		ft_putchar_fd('\n', 1);
 		i ++;
+	}*/
+	t_list *lst;
+
+	(void)argv;
+	lst = *(my_data->envlist);
+	while (lst)
+	{
+		ft_putstr_fd((char *)lst->content, 1);
+		ft_putchar_fd('\n', 1);
+		lst = lst->next;
 	}
 }
