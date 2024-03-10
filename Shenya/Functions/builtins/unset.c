903dcbf4f8ddf483fdac8f8c0d9cc282b525537e
Shenya De Silva <shenya44@outlook.com>
2024-03-10 22:33:28 +0100

    Initial commit using linkedlist

--- Shenya/Functions/builtins/unset.c
+++ Shenya/Functions/builtins/unset.c
@@ -1,15 +1,44 @@
 #include "../minishell.h"
 
+// go through the envp list - find the variable
+	// if the variable name exists in the envp list
+		// unset the variable by removing it from linked list
+	// if the variable does not exist? - do nothng
+
+int arr_len(char **arr)
+{
+	int len = 0;
+
+	if (!arr)
+		return (len);
+	while (arr[len] != NULL)
+		len ++;
+	return (len);
+}
+
 void    exec_unset(char **argv, t_envp *my_data)
 {
-	// go through the envp list
-		// if the variable name exists in the envp list
-			// if the variable after is null
-				// unset the variable by making it null
-			// if variable after is not null;
-				// find the last variable
-				// make current variable pointer point to the last variable value;
-				// unset the last variable by making it null
-	(void)argv;
-	(void)my_data;
+	t_list	*current;
+	t_list	*prev;
+	char 	**arr;
+
+	current = *(my_data->envlist);
+	prev = current;
+	while (current != NULL)
+	{
+		arr = NULL;
+      	arr = ft_split((char *)current->content, '=');
+		if (!ft_strncmp(arr[0], argv[1], ft_strlen(argv[1])))
+		{
+			prev->next = current->next;
+			ft_lstdelone(current, free_string);
+			free_arr(arr, arr_len(arr));
+			g_exit_status = 0;
+			return;
+		}
+		free_arr(arr, arr_len(arr));
+		prev = current;
+		current = current->next;
+	}
+	g_exit_status = 0;
 }
