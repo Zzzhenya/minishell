ef40f546d7b4cf362cfb728200c8af22de8bafe4
Shenya De Silva <shenya44@outlook.com>
2024-03-11 18:50:24 +0100

    Restructured the code to fit the norm

--- Shenya/Functions/builtins/exit.c
+++ Shenya/Functions/builtins/exit.c
@@ -28,45 +28,49 @@ void	print_exit_error(char *string, char *message)
 	ft_putstr_fd(message, 2);
 }
 
+static void handle_exit_codes(char **argv, int digcount)
+{
+	if (digcount && digcount <= 19 && !argv[2])
+	{
+		if (ft_atoll(argv[1]) >= 0 && ft_atoll(argv[1]) <= 255)
+			g_exit_status = ft_atoll(argv[1]);
+		else if (ft_atoll(argv[1]) < 0)
+			g_exit_status = 256 + ft_atoll(argv[1]);
+		else
+			g_exit_status = ft_atoll(argv[1]) - 256;
+		// free stuff
+		exit(g_exit_status);
+	}
+	else if (digcount && argv[2])
+	{
+		print_exit_error(NULL,"too many arguments\n");
+		g_exit_status = 1;
+		return;
+	}
+}
+
 /* Need to free stuff before exit */
 
 void	exec_exit(char **argv, t_envp *my_data)
 {
 	(void) my_data;
-	int digcount = 0;
+	int digcount;
 
+	digcount = 0;
 	ft_putstr_fd("exit\n", 2);
 	if (!argv[1])
 	{
 		// free stuff
 		exit(g_exit_status);
 	}
-	if (argv[1])
+	digcount = ft_isanumber(argv[1]);
+	if (digcount)
+		handle_exit_codes(argv, digcount);
+	else
 	{
-		digcount = ft_isanumber(argv[1]);
-		if (digcount && digcount <= 19 && !argv[2])
-		{
-			if (ft_atoll(argv[1]) >= 0 && ft_atoll(argv[1]) <= 255)
-				g_exit_status = ft_atoll(argv[1]);
-			else if (ft_atoll(argv[1]) < 0)
-				g_exit_status = 256 + ft_atoll(argv[1]);
-			else
-				g_exit_status = ft_atoll(argv[1]) - 256;
-			// free stuff
-			exit(g_exit_status);
-		}
-		else if (digcount && argv[2])
-		{
-			print_exit_error(NULL,"too many arguments\n");
-			g_exit_status = 1;
-			return;
-		}
-		else
-		{
-			print_exit_error(argv[1], ": numeric argument required\n");
-			g_exit_status = 255;
-			// free stuff
-			exit(g_exit_status);
-		}
+		print_exit_error(argv[1], ": numeric argument required\n");
+		g_exit_status = 255;
+		// free stuff
+		exit(g_exit_status);
 	}
 }
