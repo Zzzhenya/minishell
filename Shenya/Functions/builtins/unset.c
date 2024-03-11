9f0990fc5cee0a10fb40baa1b2d53dc8969f0dd6
Shenya De Silva <shenya44@outlook.com>
2024-03-11 20:06:22 +0100

    Inclueded to unset multiple variables and included test scenarios

--- Shenya/Functions/builtins/unset.c
+++ Shenya/Functions/builtins/unset.c
@@ -5,6 +5,27 @@
 		// unset the variable by removing it from linked list
 	// if the variable does not exist? - do nothng
 
+/*
+
+unset 1
+bash: unset: `1': not a valid identifier
+echo $? -> 1
+
+unset haha=hello
+bash: unset: `haha=hello': not a valid identifier
+echo $? -> 1
+
+unset non-existing-var
+echo $? -> 0
+
+unset exiting-var
+echo $? -> 0
+
+unset var1 var2  -> unsets both variables
+echo $? -> 0
+
+
+
 int arr_len(char **arr)
 {
 	int len = 0;
@@ -15,10 +36,8 @@ int arr_len(char **arr)
 		len ++;
 	return (len);
 }
-
-/* TESTTTTT */
-
-void    exec_unset(char **argv, t_envp *my_data)
+*/
+void unset_one_var(char *var, t_envp *my_data)
 {
 	t_list	*current;
 	t_list	*prev;
@@ -30,17 +49,32 @@ void    exec_unset(char **argv, t_envp *my_data)
 	{
 		arr = NULL;
       	arr = ft_split((char *)current->content, '=');
-		if (!ft_strncmp(arr[0], argv[1], ft_strlen(argv[1])))
+		if (!ft_strncmp(arr[0], var, ft_strlen(var)))
 		{
 			prev->next = current->next;
 			ft_lstdelone(current, free_string);
-			free_arr(arr, arr_len(arr));
+			free_arr(arr, get_arg_count(arr));
 			g_exit_status = 0;
 			return;
 		}
-		free_arr(arr, arr_len(arr));
+		free_arr(arr, get_arg_count(arr));
 		prev = current;
 		current = current->next;
 	}
 	g_exit_status = 0;
+}
+
+/* TESTTTTT */
+
+//void    exec_unset(char *var, t_envp *my_data)
+void    exec_unset(char **argv, t_envp *my_data)
+{
+	int i;
+
+	i = 1;
+	while (argv[i] != NULL)
+	{
+		unset_one_var(argv[i], my_data);
+		i ++;
+	}
 }
