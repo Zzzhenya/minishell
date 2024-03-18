f6f18c567bed7b9a5b017e5a60a0dfbe38964e44
Shenya De Silva <shenya44@outlook.com>
2024-03-18 19:03:44 +0100

    Signals with segmentation fault, check at school tomorrow

--- Shenya/Functions/signal.c
+++ Shenya/Functions/signal.c
@@ -1,22 +1,21 @@
 #include "minishell.h"
 
-void	handler(int sig)
+void	sig_handler(int sig)
 {
 	if (sig == SIGINT) // ctrl + c  -> new line
 	{
 		g_exit_status = sig;
 		ioctl(STDIN_FILENO, TIOCSTI, "\n");
-		rl_replace_line ("", 1);
+		rl_replace_line ("", 0);
 		rl_on_new_line();
 	}
 	//ctrl + d -> print exit, exit(1)
 	// ctrl + \ -> do nothing
 }
 
-int install_signals(void)
+void install_signals(void)
 {
 	signal(SIGINT, sig_handler);
 	//ctrl + d -> print exit, exit(1)
 	// ctrl + \ -> do nothing
-	return (0);
 }
