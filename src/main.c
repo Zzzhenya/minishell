a96cf075b9da5f7748016383a923ce0171b203e0
Shenya De Silva <sde-silv@c4c2c5.42berlin.de>
2024-03-26 16:48:20 +0100

    Modified main function to accomodate updated env structure

--- src/main.c
+++ src/main.c
@@ -28,30 +28,8 @@ char	**save_all_env_paths(char **envp)
 	perror("save_all_env_paths: ");
 	exit(EXIT_FAILURE);
 }
+/*
 
-void	interactive_bash(t_cmd **tree, char **paths, t_envp *env)
-{
-	char	user_input[1000];
-
-	install_signals();
-	if (extract_envarr(env) < 0)
-		return ;
-	/* Replace these two lines with the radline functions */
-	fgets(user_input, sizeof(user_input), stdin);
-	user_input[strcspn(user_input, "\n")] = 0;
-	tree = (t_cmd **)parse_user_input(user_input, env);
-	if (tree != NULL)
-	{
-		printf("Successfully created\n");
-		printf("flag	pipe 	type\n");
-		search_tree(*tree, paths, env);
-	}
-	else
-		printf("Error: Failed to create command tree.\n");
-	if (tree)
-		free_tree(*tree);
-	(void)paths;
-}
 
 void	non_interactive_bash(t_cmd **tree, char **paths, t_envp *env, char *script)
 {
@@ -62,37 +40,9 @@ void	non_interactive_bash(t_cmd **tree, char **paths, t_envp *env, char *script)
 	printf("NON INTERACTIVE BASH\n");
 }
 
-int	main(int argc, char **argv, char **envs)
-{
-	t_cmd	*tree;
-	t_envp	env;
-	char	**paths;
 
-	env.envarr = NULL;
-	env.cd_hist = NULL;
-	env.envlist = NULL;
-	tree = NULL;
-	env.count = 0;
-	paths = save_all_env_paths(envs);
-	if (store_envp(&env, envs) < 0)
-		return (1);
-	if (argc == 2)
-		non_interactive_bash(&tree, paths, &env, argv[1]);
-	else
-	{
-		(void)argv;
-		if (isatty(STDIN_FILENO) == 1)
-		{
-			interactive_bash(&tree, paths, &env);
-		}
-	}
-	if (env.envarr)
-		free_arr(env.envarr, env.count);
-	clear_envlist(&env);
-	free_2d(paths);
-	return (0);
-}
-/*
+*/
+
 char	*get_allocated_cwd(void)
 {
 	int		i;
@@ -129,25 +79,59 @@ void	non_interactive_mode(t_cmd **tree,
 void	interactive_mode(t_cmd **tree, char **envp, t_envp *env)
 {
 	char	*user_input;
-	char	*p_cwd;
 
-	env->cd_hist = NULL;
+	user_input = NULL;
+	install_signals();
 	while (1)
 	{
-		set_signals_interactive(-1);
-		p_cwd = get_allocated_cwd();
-		user_input = readline(p_cwd);
-		add_history(user_input);
+		user_input = readline ("Minishell > ");
 		if (!user_input)
-			exit(0);
-		*tree = parse_user_input(user_input, env);
-		search_tree(*tree, envp, env);
-		wait_each_commands(*tree);
-		free_tree(*tree);
-		free(user_input);
-		free(p_cwd);
+		{
+			g_exit_status = 0;
+			printf("exit\n");
+			// free stuff and exit
+			break;
+		}
+		if (user_input[0] != '\0')
+		{
+			add_history(user_input);
+			*tree = parse_user_input(user_input, env);
+			search_tree(*tree, envp, env);
+			//wait_each_commands(*tree);
+		}
+		if (*tree)
+			free_tree(*tree);
+		if (user_input)
+			free (user_input);
 	}
 }
+
+/*
+void	interactive_bash(t_cmd **tree, char **paths, t_envp *env)
+{
+	char	user_input[1000];
+
+	install_signals();
+	if (extract_envarr(env) < 0)
+		return ;
+	//Replace these two lines with the radline functions
+	fgets(user_input, sizeof(user_input), stdin);
+	user_input[strcspn(user_input, "\n")] = 0;
+	tree = (t_cmd **)parse_user_input(user_input, env);
+	if (tree != NULL)
+	{
+		printf("Successfully created\n");
+		printf("flag	pipe 	type\n");
+		search_tree(*tree, paths, env);
+	}
+	else
+		printf("Error: Failed to create command tree.\n");
+	if (tree)
+		free_tree(*tree);
+	(void)paths;
+}
+
+*/
 
 int	main(int argc, char **argv, char **envs)
 {
@@ -155,7 +139,12 @@ int	main(int argc, char **argv, char **envs)
 	t_envp	env;
 	char	**paths;
 
-	env.envp = envs;
+	env.envarr = NULL;
+	env.cd_hist = NULL;
+	env.envlist = NULL;
+	env.count = 0;
+	if (store_envp(&env, envs) < 0)
+		return (1);
 	(void)argv;
 	(void)argc;
 	paths = save_all_env_paths(envs);
@@ -164,5 +153,45 @@ int	main(int argc, char **argv, char **envs)
 	else if (isatty(STDIN_FILENO))
 		interactive_mode(&tree, paths, &env);
 	free_2d(paths);
+	clear_envlist(&env);
+	if (env.cd_hist != NULL)
+	{
+		free(env.cd_hist);
+		env.cd_hist = NULL;
+	}
 }
+
+/*
+
+int	main(int argc, char **argv, char **envs)
+{
+	t_cmd	*tree;
+	t_envp	env;
+	char	**paths;
+
+	env.envarr = NULL;
+	env.cd_hist = NULL;
+	env.envlist = NULL;
+	tree = NULL;
+	env.count = 0;
+	paths = save_all_env_paths(envs);
+	if (store_envp(&env, envs) < 0)
+		return (1);
+	if (argc == 2)
+		non_interactive_bash(&tree, paths, &env, argv[1]);
+	else
+	{
+		(void)argv;
+		if (isatty(STDIN_FILENO) == 1)
+		{
+			interactive_bash(&tree, paths, &env);
+		}
+	}
+	if (env.envarr)
+		free_arr(env.envarr, env.count);
+	clear_envlist(&env);
+	free_2d(paths);
+	return (0);
+}
+
 */
