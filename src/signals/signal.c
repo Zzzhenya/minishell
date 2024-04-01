c3356edc2fdaa259cf11edaf2bf2c90191d814dd
Shenya De Silva <shenya44@outlook.com>
2024-04-01 16:54:18 +0200

    Setup exit status of ctrl + c to 130

--- src/signals/signal.c
+++ src/signals/signal.c
@@ -8,10 +8,8 @@
 void	sig_handler(int sig)
 {
 	if (sig == SIGINT)
-	{
-		g_exit_status = sig;
-		//ioctl(STDIN_FILENO, TIOCSTI, "\n");		
-		//g_exit_status = 130;
+	{	
+		g_exit_status = 130;
 		ft_putchar_fd('\n', 1);
 		rl_on_new_line();
 		rl_replace_line ("", 0);
