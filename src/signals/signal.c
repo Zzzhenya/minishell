1a84a3e811f566c75837ca54bd4a60dbcd2ab99e
Shenya De Silva <shenya44@outlook.com>
2024-04-03 21:32:32 +0200

    Norm corrections

--- src/signals/signal.c
+++ src/signals/signal.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signal.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sde-silv <sde-silv@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/03 21:29:29 by sde-silv          #+#    #+#             */
+/*   Updated: 2024/04/03 21:32:15 by sde-silv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../include/minishell.h"
 
 /*
@@ -8,16 +20,11 @@
 void	sig_handler(int sig)
 {
 	if (sig == SIGINT)
-	{	
+	{
 		g_exit_status = 130;
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		ioctl(STDIN_FILENO, TIOCSTI, "\n");
-		/*
-		ft_putchar_fd('\n', 1);
-		rl_on_new_line();
-		rl_replace_line ("", 0);
-		rl_redisplay();*/
 	}
 }
 /*
@@ -28,11 +35,11 @@ void	sig_handler(int sig)
 		in sig_handler
 */
 
-void install_signals(pid_t pid)
+void	install_signals(pid_t pid)
 {
 	signal(SIGQUIT, SIG_IGN);
 	if (pid <= 0)
-		signal(SIGINT, sig_handler);	
+		signal(SIGINT, sig_handler);
 	else
-		signal(SIGINT, SIG_IGN); 
+		signal(SIGINT, SIG_IGN);
 }
