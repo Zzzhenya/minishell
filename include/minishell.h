bc54dd010ae3634b4c0617d2666ee388b13ff76b
Shenya De Silva <shenya44@outlook.com>
2024-04-04 15:27:37 +0200

    Norm error fix

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tkwak <tkwak@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 21:10:59 by tkwak             #+#    #+#             */
-/*   Updated: 2024/04/04 15:00:09 by sde-silv         ###   ########.fr       */
+/*   Updated: 2024/04/04 15:23:16 by sde-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -236,111 +236,113 @@ typedef struct s_redirec
 // [FUNCTIONS]
 /* [ LEXER ] INPUT_VALIDATE  & TOKENIZATION */
 // [ lexical_qoute.c ]
-int		count_quote(char *str, t_data *data);
-int		check_quote_arrangement(char *str, int i);
-int		check_unescaped_quote(char *str);
-int		check_quote_order(char *user_input, t_data *data, int i, int k);
+int			count_quote(char *str, t_data *data);
+int			check_quote_arrangement(char *str, int i);
+int			check_unescaped_quote(char *str);
+int			check_quote_order(char *user_input, t_data *data, int i, int k);
 
 // [ lexical_chopping.c ]
-char	*case_s_quote(t_data *data, char *str, int i, int j);
-char	*case_d_quote(t_data *data, char *str, int i, int j);
-int		check_str_null(t_data *data, char *str, int i, int j);
-int		ft_chopper(t_data *data, char *str, int j);
+char		*case_s_quote(t_data *data, char *str, int i, int j);
+char		*case_d_quote(t_data *data, char *str, int i, int j);
+int			check_str_null(t_data *data, char *str, int i, int j);
+int			ft_chopper(t_data *data, char *str, int j);
 
 // [ lexical_expanding.c ]
-int		find_matching_env_row(char *str, char **env);
-char	*trim_single_quotes(char *str);
-int		remove_single_quotes_from_token(t_data *data, int i);
-char	*replace_substring(char *token,
-			char *row_matched_env, int column_index_dollar);
-int		expand_env(t_data *data, char **env, int i);
+int			find_matching_env_row(char *str, char **env);
+char		*trim_single_quotes(char *str);
+int			remove_single_quotes_from_token(t_data *data, int i);
+char		*replace_substring(char *token,
+				char *row_matched_env, int column_index_dollar);
+int			expand_env(t_data *data, char **env, int i);
 
 // [ lexical_validating.c ]
-void	toggle_inword_inquote(int *flag_inword, int *n_word, int *flag_inquote);
-int		count_word(const char *str,
-			int n_word, int flag_inword, int flag_inquote);
-char	**validate_input(char *user_input, char **env);
+void		toggle_inword_inquote(int *flag_inword,
+				int *n_word, int *flag_inquote);
+int			count_word(const char *str,
+				int n_word, int flag_inword, int flag_inquote);
+char		**validate_input(char *user_input, char **env);
 
 // [ lexical_analysis.c ]
-int		check_token_length(int *token);
-int		check_redirections(char *str);
-int		check_token_type(char *str);
-int		count_row_2d_array(char **array);
-int		*token_malloc(char **after_chopping);
+int			check_token_length(int *token);
+int			check_redirections(char *str);
+int			check_token_type(char *str);
+int			count_row_2d_array(char **array);
+int			*token_malloc(char **after_chopping);
 
 // [ change_exit_status.c ] //
-void	if_dollar_q_mark(char **tmp, char *status, int *flag, int *i);
-void	if_tmp(int *flag, char *tmp, char **argv);
-char	*convert_status_to_ascii(void);
-void	replace_exit_status(char ***argv, int i, int flag, int j);
+void		if_dollar_q_mark(char **tmp, char *status, int *flag, int *i);
+void		if_tmp(int *flag, char *tmp, char **argv);
+char		*convert_status_to_ascii(void);
+void		replace_exit_status(char ***argv, int i, int flag, int j);
 
 /* [ PASER ]*/
 // [ syntax_analysis.c ]
-int		syntax_simple_cmd(char **cmd_line, int *token, int *i, t_cmd **node);
-int		syntax_simple_redirect(char **cmd_line, int *i, t_cmd **node);
-int		syntax_redirects(char **cmd_line, int *token, int *i, t_cmd **node);
-int		syntax_cmds(char **cmd_line, int *token, int *i, t_cmd **node);
-int		syntax_pipe(char **cmd_line, int *token, int *i, t_cmd **node);
+int			syntax_simple_cmd(char **cmd_line,
+				int *token, int *i, t_cmd **node);
+int			syntax_simple_redirect(char **cmd_line, int *i, t_cmd **node);
+int			syntax_redirects(char **cmd_line, int *token, int *i, t_cmd **node);
+int			syntax_cmds(char **cmd_line, int *token, int *i, t_cmd **node);
+int			syntax_pipe(char **cmd_line, int *token, int *i, t_cmd **node);
 
 // [ syntax_create_node.c ]
-t_cmd	*create_tree_node(int type, int pipe_index);
-char	**copy_substring_range(char **cmd_line, int start, int end);
-t_cmd	*create_leaf_node(char **cmd_line, int type, int start, int end);
+t_cmd		*create_tree_node(int type, int pipe_index);
+char		**copy_substring_range(char **cmd_line, int start, int end);
+t_cmd		*create_leaf_node(char **cmd_line, int type, int start, int end);
 
 // [ syntax_find_target.c ]
-int		find_target_index(int *token, int target, int *i);
-int		find_index_pipe(int *token, int *i);
-int		find_index_redirec(int *token, int *i);
-int		find_index_next_redirec(int *token, int *i);
-void	update_pipe_index(int *i, int pipe_index, int tmp);
+int			find_target_index(int *token, int target, int *i);
+int			find_index_pipe(int *token, int *i);
+int			find_index_redirec(int *token, int *i);
+int			find_index_next_redirec(int *token, int *i);
+void		update_pipe_index(int *i, int pipe_index, int tmp);
 
 // [ get_cmds.c ]
-t_cmd	*parse_user_input(char *user_input, t_envp *env);
+t_cmd		*parse_user_input(char *user_input, t_envp *env);
 
 /* [ UTILS ] */
 // [ util_skip.c ]
-int		ft_isspace(char c);
-void	skip_normal_char(char *str, int *i);
-void	skip_space_in_str(char *str, int *i);
-char	*strjoin_c(char *str, char c);
-char	*strrjoin(char *s1, char *s2);
+int			ft_isspace(char c);
+void		skip_normal_char(char *str, int *i);
+void		skip_space_in_str(char *str, int *i);
+char		*strjoin_c(char *str, char c);
+char		*strrjoin(char *s1, char *s2);
 
 // [ util_str.c ]
-int		ft_strchr_m(char *str, char c);
-int		ft_strcmp(char *s1, char *s2);
-int		ft_putstr_m(char *str);
-int		ft_strcpy_m1(t_data *data, char *str, int len, int j);
-int		ft_strcpy_m2(t_data *data, char *str, int len, int j);
+int			ft_strchr_m(char *str, char c);
+int			ft_strcmp(char *s1, char *s2);
+int			ft_putstr_m(char *str);
+int			ft_strcpy_m1(t_data *data, char *str, int len, int j);
+int			ft_strcpy_m2(t_data *data, char *str, int len, int j);
 
 // [ util_str2.c ]
-char	*strjoin_c(char *str, char c);
-char	*strrjoin(char *s1, char *s2);
+char		*strjoin_c(char *str, char c);
+char		*strrjoin(char *s1, char *s2);
 
 // [ util_error.c ]
-void	syntax_pipe_error_print(void);
+void		syntax_pipe_error_print(void);
 
 // [ util_free.c ]
-void	free_2d(char **arr);
-void	free_stdios(t_redirec *stdios);
-void	free_tree(t_cmd	*tree);
-void	freeing_norminette(char **validated_input, int *token);
-void	free_arr(char **arr, int loc);
+void		free_2d(char **arr);
+void		free_stdios(t_redirec *stdios);
+void		free_tree(t_cmd	*tree);
+void		freeing_norminette(char **validated_input, int *token);
+void		free_arr(char **arr, int loc);
 
 /* [ STARTING POINT ] */
 // [ main.c ]
-char	**save_all_env_paths(char **envp);
-char	*get_allocated_cwd(void);
-void	interactive_mode(t_cmd **tree, char **envp, t_envp *env);
-void	non_interactive_mode(t_cmd **tree,
-			char *input, char **envp, t_envp *env);
-int		main(int argc, char **argv, char **envs);
+char		**save_all_env_paths(char **envp);
+char		*get_allocated_cwd(void);
+void		interactive_mode(t_cmd **tree, char **envp, t_envp *env);
+void		non_interactive_mode(t_cmd **tree,
+				char *input, char **envp, t_envp *env);
+int			main(int argc, char **argv, char **envs);
 
 /* [ ENV ] */
 // [ envp_actions.c ]
-int		store_envp(t_envp *env, char **envs);
-void	clear_envlist(t_envp *env);
-int		extract_envarr(t_envp *my_data);
-void	free_string(void *input);
+int			store_envp(t_envp *env, char **envs);
+void		clear_envlist(t_envp *env);
+int			extract_envarr(t_envp *my_data);
+void		free_string(void *input);
 
 // [ Executing ]
 /*
@@ -348,63 +350,63 @@ void	free_string(void *input);
 	wait_each_commands
 */
 // [executing.c]
-void	print_error_cmd(t_cmd *file_path, char **envp);
-void	exec(char **cmd, char **env, t_envp *envo);
+void		print_error_cmd(t_cmd *file_path, char **envp);
+void		exec(char **cmd, char **env, t_envp *envo);
 // [search_tree.c]
-void	search_tree(t_cmd *node, char **envp, t_envp *env);
+void		search_tree(t_cmd *node, char **envp, t_envp *env);
 
 // [ REDIRECTIONS ]
 // [setup_redirections.c]
-void	setup_redirections(t_redirec *stdios);
+void		setup_redirections(t_redirec *stdios);
 
 // [ETC ]
 t_redirec	*find_last(t_redirec *stdios, char c, t_redirec *last);
 
 // [here_doc_functions.c]
-void	heredoc_input(int fd, char *word);
+void		heredoc_input(int fd, char *word);
 
 // util_debug.c
-void	print_tree(t_cmd *node);
-int		get_arg_count(char **argv);
+void		print_tree(t_cmd *node);
+int			get_arg_count(char **argv);
 
 // [ SIGNAL ]
 // [signals.c]
-void	install_signals(pid_t pid);
+void		install_signals(pid_t pid);
 
 // [ BUILT-IN ] 
 // [cd.c]
-void	exec_cd(char **argv, t_envp *my_data, char *path);
+void		exec_cd(char **argv, t_envp *my_data, char *path);
 
 // [env.c]
-void	exec_env(char **argv, t_envp *my_data);
+void		exec_env(char **argv, t_envp *my_data);
 
 // [pwd.c]
-void	exec_pwd(void);
-char	*get_pwd(void);
+void		exec_pwd(void);
+char		*get_pwd(void);
 
 // [echo.c]
-void	exec_echo(char **argv, int count, char **arr);
+void		exec_echo(char **argv, int count, char **arr);
 
 // [exit.c]
-void	exec_exit(char **argv, t_envp *my_data);
+void		exec_exit(char **argv, t_envp *my_data);
 
 // [unset.c]
-void	exec_unset(char **argv, t_envp *my_data);
-void	unset_one_var(char *var, t_envp *my_data);
+void		exec_unset(char **argv, t_envp *my_data);
+void		unset_one_var(char *var, t_envp *my_data);
 
 // [export.c]
-void	exec_export(char **argv, t_envp *my_data);
+void		exec_export(char **argv, t_envp *my_data);
 
 // [export_utils.c]
-void	print_export_error(char *var, char *val, char *message);
-int		is_valid_var_start(unsigned char c);
-int		is_valid_var_char(char *var);
-void	print_variables_list(char **arr);
-void	export_one_var(char **arr, t_envp *my_data);
+void		print_export_error(char *var, char *val, char *message);
+int			is_valid_var_start(unsigned char c);
+int			is_valid_var_char(char *var);
+void		print_variables_list(char **arr);
+void		export_one_var(char **arr, t_envp *my_data);
 
 // [builtin_utils.c]
-char	**strip_empty_strings(char **cmdstr);
-int		count_non_empty_strings(char **cmdstr);
+char		**strip_empty_strings(char **cmdstr);
+int			count_non_empty_strings(char **cmdstr);
 
 // [MEMO]
 /*
@@ -416,22 +418,22 @@ File descriptor of STDIN, relating with input source like Key-board.
 */
 
 // [redirection_error_handle.c]
-int		redirection_error_handle(t_cmd *type, pid_t pid);
+int			redirection_error_handle(t_cmd *type, pid_t pid);
 
 // [child_process.c]
-void	pid_zero_exec(t_cmd *cmd, char **envp, t_envp *env, pid_t pid);
+void		pid_zero_exec(t_cmd *cmd, char **envp, t_envp *env, pid_t pid);
 
 // [wait_blocked_cmds.c]
-void	wait_each_command(t_cmd *tree);
-int		count_commands(t_cmd *tree);
+void		wait_each_command(t_cmd *tree);
+int			count_commands(t_cmd *tree);
 
 // [route_builtins.c]
-void	builtin_action(t_cmd *builtin, char **cmdline, t_envp *env);
-int		check_builtin(t_cmd *file_path);
-void	pid_pid_builtin_n_set(t_cmd *cmd, t_envp *env, pid_t pid);
+void		builtin_action(t_cmd *builtin, char **cmdline, t_envp *env);
+int			check_builtin(t_cmd *file_path);
+void		pid_pid_builtin_n_set(t_cmd *cmd, t_envp *env, pid_t pid);
 
 /* readline */
-void	rl_replace_line(const char *text, int clear_undo);
-void	execute_simple_cmd(t_cmd *cmd, t_redirec **stdios,
-			char **envp, t_envp *env);
+void		rl_replace_line(const char *text, int clear_undo);
+void		execute_simple_cmd(t_cmd *cmd, t_redirec **stdios,
+				char **envp, t_envp *env);
 #endif
