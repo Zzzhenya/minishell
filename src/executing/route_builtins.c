95f0844b35fb58f3bed73c30c54e66148cd1b9c1
Shenya De Silva <shenya44@outlook.com>
2024-04-09 22:01:03 +0200

    Removed unused builtin router functions

--- src/executing/route_builtins.c
+++ src/executing/route_builtins.c
@@ -12,38 +12,6 @@
 
 #include "../../include/minishell.h"
 
-/* 
-	Builtin router for parent process
-*/
-void	pid_pid_builtin_n_set(t_cmd *cmd, t_envp *env, pid_t pid)
-{
-	install_signals(1);
-	if (redirection_error_handle(cmd->l_child, pid))
-		return ;
-	if (!ft_strcmp(cmd->r_child->cmdstr[0], "exit"))
-	{
-		exec_exit(cmd->r_child->cmdstr, env);
-		return ;
-	}
-	else if (!ft_strcmp(cmd->r_child->cmdstr[0], "unset"))
-	{
-		exec_unset(cmd->r_child->cmdstr, env);
-		return ;
-	}
-	else if (!ft_strcmp(cmd->r_child->cmdstr[0], "export"))
-	{
-		exec_export(cmd->r_child->cmdstr, env);
-		return ;
-	}
-	else if (!ft_strcmp(cmd->r_child->cmdstr[0], "cd"))
-	{
-		exec_cd(cmd->r_child->cmdstr, env, NULL);
-		return ;
-	}
-	if (access(cmd->r_child->cmdstr[0], X_OK) != 0)
-		g_exit_status = 127;
-}
-
 int	check_builtin(t_cmd *file_path)
 {
 	if (!ft_strcmp(file_path->cmdstr[0], "echo")
@@ -60,24 +28,8 @@ int	check_builtin(t_cmd *file_path)
 		return (0);
 }
 
-/*
-	builtin router for child processes
-*/
-void	builtin_action(t_cmd *builtin, char **cmdline, t_envp *env)
-{
-	if (!ft_strcmp(builtin->cmdstr[0], "echo"))
-		exec_echo(cmdline, 0, NULL);
-	else if (!ft_strcmp(builtin->cmdstr[0], "pwd"))
-		exec_pwd();
-	else if (!ft_strcmp(builtin->cmdstr[0], "env"))
-		exec_env(NULL, env);
-	exit(errno);
-}
-
 void	builtin_router(t_cmd *cmd, t_envp *env, pid_t pid)
 {
-	if (pid != 0)
-		install_signals(1);
 	if (redirection_error_handle(cmd->l_child, pid))
 	{
 		if (pid != 0)
@@ -102,7 +54,5 @@ void	builtin_router(t_cmd *cmd, t_envp *env, pid_t pid)
 	if (pid == 0)
 		exit(g_exit_status);
 	else
-	{
 		return ;
-	}
 }
