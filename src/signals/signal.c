1145a4e4c3ef8453bbd5cebcde325a53ca357467
Shenya De Silva <shenya44@outlook.com>
2024-04-10 16:47:30 +0200

    Norm corrections

--- src/signals/signal.c
+++ src/signals/signal.c
@@ -6,7 +6,7 @@
 /*   By: sde-silv <sde-silv@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 21:29:29 by sde-silv          #+#    #+#             */
-/*   Updated: 2024/04/03 21:32:15 by sde-silv         ###   ########.fr       */
+/*   Updated: 2024/04/10 16:46:59 by sde-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,53 +22,53 @@ void	handle_nl(int sig)
 	process after executing, when a
 	child process hangs
 */
-void	install_signal_hang()
+void	install_signal_hang(void)
 {
-	struct sigaction act1;
-    struct sigaction act2;
+	struct sigaction	act1;
+	struct sigaction	act2;
 
-    ft_bzero(&act1, sizeof(act1));
-    ft_bzero(&act2, sizeof(act2));
-    act1.sa_handler = SIG_IGN;
-    act2.sa_handler = &handle_nl;
-    sigaction(SIGQUIT, &act1, NULL);
-    sigaction(SIGINT, &act2, NULL);
+	ft_bzero(&act1, sizeof(act1));
+	ft_bzero(&act2, sizeof(act2));
+	act1.sa_handler = SIG_IGN;
+	act2.sa_handler = &handle_nl;
+	sigaction(SIGQUIT, &act1, NULL);
+	sigaction(SIGINT, &act2, NULL);
 }
 
 void	handle_prompt(int sig)
 {
 	(void)sig;
-    write (1, "\n", 1);
+	write (1, "\n", 1);
 	rl_on_new_line();
 	rl_replace_line ("", 0);
-    rl_redisplay();
+	rl_redisplay();
 }
 
 /* 	signal definitions for the main 
 	process and child after executing tree
 */
-void	install_signals_main()
+void	install_signals_main(void)
 {
-	struct sigaction act1;
-    struct sigaction act2;
+	struct sigaction	act1;
+	struct sigaction	act2;
 
-    ft_bzero(&act1, sizeof(act1));
-    ft_bzero(&act2, sizeof(act2));
-    act1.sa_handler = SIG_IGN;
-    act2.sa_handler = &handle_prompt;
-    sigaction(SIGQUIT, &act1, NULL);
-    sigaction(SIGINT, &act2, NULL);
+	ft_bzero(&act1, sizeof(act1));
+	ft_bzero(&act2, sizeof(act2));
+	act1.sa_handler = SIG_IGN;
+	act2.sa_handler = &handle_prompt;
+	sigaction(SIGQUIT, &act1, NULL);
+	sigaction(SIGINT, &act2, NULL);
 }
 
-void	install_signals_child()
+void	install_signals_child(void)
 {
-	struct sigaction act1;
-	struct sigaction act2;
-    ft_bzero(&act1, sizeof(act1));
-    ft_bzero(&act2, sizeof(act2));
-    act1.sa_handler = SIG_IGN;
-    act2.sa_handler = SIG_IGN;
-    sigaction(SIGQUIT, &act1, NULL);
-    sigaction(SIGINT, &act2, NULL);
+	struct sigaction	act1;
+	struct sigaction	act2;
 
+	ft_bzero(&act1, sizeof(act1));
+	ft_bzero(&act2, sizeof(act2));
+	act1.sa_handler = SIG_IGN;
+	act2.sa_handler = SIG_IGN;
+	sigaction(SIGQUIT, &act1, NULL);
+	sigaction(SIGINT, &act2, NULL);
 }
