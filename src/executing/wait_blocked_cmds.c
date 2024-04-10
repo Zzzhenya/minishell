b54d8cf64fcb11832431f17352fa9afa69f124ad
Shenya De Silva <shenya44@outlook.com>
2024-04-10 16:21:51 +0200

    Norm corrections, remove commented out code

--- src/executing/wait_blocked_cmds.c
+++ src/executing/wait_blocked_cmds.c
@@ -6,7 +6,7 @@
 /*   By: sde-silv <sde-silv@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 16:09:31 by sde-silv          #+#    #+#             */
-/*   Updated: 2024/04/03 16:10:16 by sde-silv         ###   ########.fr       */
+/*   Updated: 2024/04/10 16:18:17 by sde-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,29 +45,14 @@ int	count_commands(t_cmd *tree)
 */
 void	wait_each_command(t_cmd *tree, t_envp *env)
 {
-	int status;
-	/*
-	int	count;
-	int	i;
-	int status;
+	int	status;
 
-	i = 0;
-	count = 0;*/
 	(void)tree;
 	if (env->cmds == 1 && env->builtin == 1)
 		return ;
-	while (waitpid(-1, &status, WUNTRACED) > 0)
+	while (waitpid(-1, &stastus, WUNTRACED) > 0)
 	{
 		if (WIFEXITED(status))
 			g_exit_status = WEXITSTATUS(status);
-
 	}
-	/*
-	count = count_commands(tree);
-	while (i < count)
-	{
-		waitpid(-1, &status, WUNTRACED);
-		i ++;
-	}*/
-	
 }
