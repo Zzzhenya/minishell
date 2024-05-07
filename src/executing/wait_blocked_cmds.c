9e84fab3d2e2bcaeef0f91bda0101c6c59a0451f
Shenya De Silva <sde-silv@c4b12c5.42berlin.de>
2024-05-07 21:11:35 +0200

    Cleaning up wait blocked commands

--- src/executing/wait_blocked_cmds.c
+++ src/executing/wait_blocked_cmds.c
@@ -42,42 +42,16 @@ int	count_commands(t_cmd *tree)
 	WUNTRACED - 
 		return info about terminated children
 		return info when child is stopped by signal
-*/
-void	wait_each_command(t_cmd *tree, t_envp *env)
-{
-	int	status;
-	int i;
-	int sig = 0;
 
-	(void)tree;
-	status = 0;
-	i = 0;
-	/*if (env->cmds == 1 && env->builtin == 1)
-		return ;*/
-	if (env->procs == 1 && env->builtin == 1)
-		return;
-	/*
 	while (waitpid(-1, &status, WUNTRACED) > 0)
 	{
 		if (WIFEXITED(status))
 			g_exit_status = WEXITSTATUS(status);
-	}*/
-	while (i < env->procs)
-	{
-		waitpid(env->arr[i].pid, &status, 0);
-		if (WIFEXITED(status))
-			env->arr[i].status = WEXITSTATUS(status);
-		else if (WIFSIGNALED(status))
-		{
-			sig = WTERMSIG(status);
-			env->arr[i].status  = sig;
-		}
-		//printf("pid: %d , status: %d %s\n", env->arr[i].pid, env->arr[i].status, strsignal(status));
-		i ++;
 	}
-	g_exit_status = env->arr[i - 1].status;
-	//printf("status %d\n", g_exit_status);
-	if ((sig == 2 || sig == 3)) // (env->cmds != 1) && 
+*/
+static void	handle_sig_numbers(int sig, int status)
+{
+	if ((sig == 2 || sig == 3))
 	{
 		if (sig == 2)
 		{
@@ -90,4 +64,32 @@ void	wait_each_command(t_cmd *tree, t_envp *env)
 			g_exit_status = status;
 		}
 	}
+}
+
+void	wait_each_command(t_cmd *tree, t_envp *env)
+{
+	int	status;
+	int	i;
+	int	sig;
+
+	(void)tree;
+	status = 0;
+	i = 0;
+	sig = 0;
+	if (env->procs == 1 && env->builtin == 1)
+		return ;
+	while (i < env->procs)
+	{
+		waitpid(env->arr[i].pid, &status, 0);
+		if (WIFEXITED(status))
+			env->arr[i].status = WEXITSTATUS(status);
+		else if (WIFSIGNALED(status))
+		{
+			sig = WTERMSIG(status);
+			env->arr[i].status = sig;
+		}
+		i ++;
+	}
+	g_exit_status = env->arr[i - 1].status;
+	handle_sig_numbers(sig, status);
 }
