5cbd013dc2266bc03e71dee6db533b303979d0a6
Zzzhenya <shenya44@outlook.com>
2024-05-13 00:14:35 +0200

    Fixed the leak in cd ~

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -117,7 +117,7 @@ static char	*get_home_join(t_envp *my_data, char **argv, char *path)
 	temp = change_to_home(my_data);
 	if (!temp)
 		return (NULL);
-	path = ft_strjoin(temp, ft_strdup(&argv[1][1]));
+	path = ft_strjoin(temp, (&argv[1][1]));
 	free (temp);
 	return (path);
 }
