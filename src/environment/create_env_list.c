2866d93b45206457b24193676dfa02d8c217c297
Shenya De Silva <sde-silv@c4b12c5.42berlin.de>
2024-05-13 15:19:41 +0200

    Function to check for SHLVL and PATH and set the value

--- src/environment/create_env_list.c
+++ src/environment/create_env_list.c
@@ -26,12 +26,62 @@ static char	*increment_shlvl(char *str)
 	ret = NULL;
 	num = 0;
 	val = ft_strchr(str, '=');
-	num = ft_atoi(val + 1);
+	if (val[1] != '\0')
+		num = ft_atoi(val + 1);
+	else
+		num = ft_atoi("0");
 	ret = ft_itoa(num + 1);
 	val = ft_strjoin("SHLVL=", ret);
 	free (ret);
 	free (str);
 	return (val);
+}
+
+t_list *find_variable(t_envp *env, char *search, int len)
+{
+	t_list	*current;
+	t_list	*var;
+
+	var = NULL;
+	current = NULL;
+	current = env->envlist;
+	while (current != NULL)
+	{
+		if (ft_strncmp(current->content, search, len) == 0)
+		{
+			var = current;
+			break ;
+		}
+		current = current->next;
+	}
+	return (var);
+}
+
+/*
+	set SHLVL value
+		if SHLVL with value increment it
+		if SHLVL without value set it to 1
+		if not SHLVL add SHLVL=1 to end of list
+	set PATH value
+*/
+void	set_basic_vals(t_envp *env)
+{
+	t_list	*var;
+	char	*path;
+
+	var = NULL;
+	path = NULL;
+	var = find_variable(env, "SHLVL=", 6);
+	if (var)
+		var = increment_shlvl(var->content);
+	else
+		ft_lstadd_back(&env->envlist, ft_lstnew(increment_shlvl(ft_strdup("SHLVL="))));
+	var = find_variable(env, "PATH=", 5);
+	if (!var)
+	{
+		path = ft_strjoin("PATH=", "/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:");
+		ft_lstadd_back(&env->envlist, ft_lstnew(path));
+	}
 }
 
 /*
@@ -53,12 +103,11 @@ int	store_envp(t_envp *env, char **envs)
 			clear_envlist(env);
 			return (-1);
 		}
-		if (ft_strncmp(temp, "SHLVL=", 6) == 0)
-			temp = increment_shlvl(temp);
 		ft_lstadd_back(&env->envlist, ft_lstnew(temp));
 		temp = NULL;
 		env->count ++;
 		i ++;
 	}
+	set_basic_vals(env);
 	return (0);
 }
