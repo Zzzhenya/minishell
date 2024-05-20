7b02a9433a2ec5b89b7e4f9f14048a8cf80da8b4
Shenya De Silva <sde-silv@c4b12c5.42berlin.de>
2024-05-20 18:52:52 +0200

    Reduce line number

--- src/parsing/lexical_expanding_tool4.c
+++ src/parsing/lexical_expanding_tool4.c
@@ -46,6 +46,50 @@ char	*replace_substring_special(t_expand *data, char **env, int len)
 	return (res);
 }
 
+int	cpy_until_dollar(char *res, int *i_dollar, char *token)
+{
+	int	i;
+
+	i = 0;
+	while (i < *i_dollar)
+	{
+		res[i] = token[i];
+		i++;
+	}
+	return (i);
+}
+
+int	cpy_env_val(char *res, char *row_env, int *i_new, int i)
+{
+	while (row_env[*i_new] != '\0')
+	{
+		res[i] = row_env[*i_new];
+		i++;
+		*i_new++;
+	}
+	return (i);
+}
+/*
+	while (token[i_dollar + tmp + 1] != '\0')
+	{
+		res[i] = token[i_dollar + tmp + 1];
+		i++;
+		tmp++;
+	}
+
+*/
+
+int	cpy_after_env_var(char *token, char *res, int j, int i)
+{
+	while (token[j] != '\0')
+	{
+		res[i] = token[j];
+		i++;
+		j++;
+	}
+	return (i);
+}
+
 char	*replace_substring_1(char *token, char *row_env, int i_dollar)
 {
 	int		i;
@@ -60,23 +104,9 @@ char	*replace_substring_1(char *token, char *row_env, int i_dollar)
 				+ i_dollar + 1 + 100) * sizeof(char));
 	if (res == NULL)
 		return (NULL);
-	while (i < i_dollar)
-	{
-		res[i] = token[i];
-		i++;
-	}
-	while (row_env[i_new] != '\0')
-	{
-		res[i] = row_env[i_new];
-		i++;
-		i_new++;
-	}
-	while (token[i_dollar + tmp + 1] != '\0')
-	{
-		res[i] = token[i_dollar + tmp + 1];
-		i++;
-		tmp++;
-	}
+	i = cpy_until_dollar(res, &i_dollar, token);
+	i = cpy_env_val(res, row_env, &i_new, i);
+	i = cpy_after_env_var(token, res, (i_dollar + tmp + 1), i);
 	res[i] = '\0';
 	free(token);
 	return (res);
